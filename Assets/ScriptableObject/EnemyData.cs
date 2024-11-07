0d667a5a96b9f6bee81d52c39d2bd14bc655363a
Cooper Fleishman <csfprogramming@gmail.com>
2024-11-06 19:13:27 -0500

    moved current health into each enemy instance (fixed the bug we had on 0.0.1)

--- Assets/ScriptableObject/EnemyData.cs
+++ Assets/ScriptableObject/EnemyData.cs
@@ -8,14 +8,7 @@ public class EnemyData : ScriptableObject
     [SerializeField] private int attackDamage;
     // Reward on death todo after initial demo
 
-    private int currentHealth;
-
-    public int CurrentHealth { get { return currentHealth; } set { currentHealth = value; } }
     public int MoveSpeed { get { return moveSpeed; } set { moveSpeed = value; } }
     public int AttackDamage { get { return attackDamage; } set { attackDamage = value; } }
-
-    private void Awake()
-    {
-        currentHealth = maxHealth;
-    }
+    public int MaxHealth { get { return maxHealth; } set { maxHealth = value; } }
 }
