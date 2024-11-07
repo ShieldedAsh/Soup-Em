24426dc3008c0bde0b5f532f666c8a7e96e7be70
Cooper Fleishman <csfprogramming@gmail.com>
2024-11-07 14:24:10 -0500

    moved the UpgradeType enum out of the Upgrade class (put it in the wrong spot oops)

--- Assets/ScriptableObject/Upgrade.cs
+++ Assets/ScriptableObject/Upgrade.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
 
+
+
+public enum UpgradeType
+{
+    Health,
+    Damage
+}
+
 [CreateAssetMenu(fileName = "Upgrade", menuName = "Scriptable Objects/Upgrade")]
 public class Upgrade : ScriptableObject
 {
-    public enum UpgradeType
-    {
-        Health,
-        Damage
-    }
-
     [SerializeField] private UpgradeType type; // type of upgrade (code is hella smelly but eh, who cares for this proj)
     [SerializeField] private int value; // what value to apply in the upgrade
     [SerializeField] private int cost; // how much the upgrade costs
